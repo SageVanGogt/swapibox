7490a589c8dfbbe127ef171306e366e3e5eca8a0
Sage Vogt <thurmanvogt@gmail.com>
2018-05-08 13:48:05 -0600

    Update button snapshots

--- src/Button/index.js
+++ src/Button/index.js
@@ -5,7 +5,7 @@ const Button = (props) => {
   const { section, handleClickEvent } = props
   return (
     <div className='button'>
-      <button name={section} onClick={(event) => handleClickEvent(event)}>
+      <button name={section} onClick={handleClickEvent}>
         {section}
       </button>
     </div>
