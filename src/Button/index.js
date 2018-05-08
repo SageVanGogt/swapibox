8ebd0e6c1bf44f47cf0e262fe63ce86ebd95cf3f
Sage Vogt <thurmanvogt@gmail.com>
2018-05-08 11:37:09 -0600

    Add event handler and subsequent test to button component

--- src/Button/index.js
+++ src/Button/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 // import './index.css';
 
 const Button = (props) => {
+  const { section, handleClickEvent } = props
   return (
     <div className='button'>
-      <button name={props.section}>
-        {props.section}
+      <button name={section} onClick={(event) => handleClickEvent(event)}>
+        {section}
       </button>
     </div>
   );
--- src/Button/index.test.js
+++ src/Button/index.test.js
@@ -8,11 +8,23 @@ describe('Button component', () => {
     const actual = shallow(<Button />)
 
     expect(actual).toMatchSnapshot();
-  })
+  });
 
   it('should render the apppropriate button depending on which section prop is passed in', () => {
     const actual = shallow(<Button section={'vehicles'}/>)
-    
+
     expect(actual).toMatchSnapshot();
-  })
+  });
+
+  it('should call handleClickEvent onClick', () => {
+    const handleClickEvent = jest.fn();
+    const mockEvent = {target: {name: 'vehicles'}};
+    const wrapper = shallow(<Button 
+      section={'vehicles'} 
+      handleClickEvent={handleClickEvent}
+    />);
+
+    wrapper.find('button').simulate('click', mockEvent);
+    expect(handleClickEvent).toBeCalledWith(mockEvent);
+  });
 })
