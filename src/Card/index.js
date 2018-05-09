c13f3a72af6914f655bd81cddcec3a4d2b5e3a2d
Sage Vogt <thurmanvogt@gmail.com>
2018-05-08 20:44:35 -0600

    Add boilerplate card text and test file

---
+++ src/Card/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = (props) => {
+  return(
+    <div className="category-card">
+      
+    </div>
+  );
+}
+
+Card.propTypes = {
+  
+}
+
+export default Card;
